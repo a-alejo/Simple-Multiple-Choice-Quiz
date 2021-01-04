e37c9d1069a999adbb226a93827d0412bd97c2da
aalejo92 <76714792+aalejo92@users.noreply.github.com>
2021-01-04 10:58:27 -0500

    Add files via upload

---
+++ Question.py
@@ -0,0 +1,6 @@
+# Create a multiple choice question
+
+class Question:
+    def __init__(self, prompt, answer):
+        self.prompt = prompt
+        self.answer = answer
---
+++ app.py
@@ -0,0 +1,27 @@
+from Question import Question
+
+question_prompts = [
+    'Which of the below option is an odd number?\n(a) 1\n(b) 4\n(c) 12\n(d) 22\n ',
+    'What is New Mexico?\n(a) A country\n(b) A city\n(c) A state\n(d) An island\n ',
+    '3/4 is an example of which of these?\n(a) A decimal\n(b) A fraction\n(c) An odd number\n(d) A rational number\n ',
+    'The planet closest to the sun is...?\n(a) Earth\n(b) Mercury\n(c) Jupiter\n(d) Mars\n ',
+]
+# array of questions that can later be looped
+questions = [
+    Question(question_prompts[0], 'a'),
+    Question(question_prompts[1], 'c'),
+    Question(question_prompts[2], 'b'),
+    Question(question_prompts[3], 'b'),
+
+
+]
+# function to run the test and checks responses
+def run_test(questions):
+    score = 0
+    for question in questions:
+        answer =input(question.prompt)
+        if answer == question.answer:
+            score += 1
+    print('You got '+ str(score) + '/' + str(len(questions)) + ' correct!')
+
+run_test(questions)
